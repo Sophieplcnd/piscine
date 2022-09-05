3e05f54cd5a44a9afae3b1cfe6353f0f47369a7a
Your Name <you@example.com>
2022-09-05 15:37:38 +0100

    my very first commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,1 +1,2 @@
-curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"sp\"}}){id}}"}'
+curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"sp\"}}){id}}"}' |cut -d ":" -f4 | cut -d "}" -f1
+
