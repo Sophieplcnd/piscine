2f865a8d74db32ed56525e0f4b2d235adc3ddefd
Your Name <you@example.com>
2022-09-07 18:47:41 +0100

    honeybb

---
+++ printcomb2.go
@@ -0,0 +1,18 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '9'; i++ {
+		for j := i + 1; j <= '9'; j++ 
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				if i < 55 {
+					z01.PrintRune(44)
+					z01.PrintRune(32)
+				}
+		
+		}
+	}
+	z01.PrintRune('\n')
+}
