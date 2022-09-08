515b59e8c3ba4042f0388e8eb3dc1d283cdd4d5b
Your Name <you@example.com>
2022-09-08 13:19:53 +0100

    honeybb

--- printcomb2.go
+++ printcomb2.go
@@ -1,8 +1,8 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
-func PrintComb2() {
+func main() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
@@ -10,12 +10,12 @@ func PrintComb2() {
 					if k > i || (k == i && l > j) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
-						if i < 55 {
-							z01.PrintRune(32)
-						}
+						z01.PrintRune(32)
 						z01.PrintRune(k)
 						z01.PrintRune(l)
-						if i < 55 {
+						if i == 57 && j == 56 {
+							z01.PrintRune('\n')
+						} else {
 							z01.PrintRune(44)
 							z01.PrintRune(32)
 						}
@@ -24,5 +24,4 @@ func PrintComb2() {
 			}
 		}
 	}
-	z01.PrintRune('\n')
 }
