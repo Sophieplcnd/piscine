75d54590b0132391d170e435b283631893553047
Your Name <you@example.com>
2022-09-08 11:55:46 +0100

    honeybb

--- printcomb2.go
+++ printcomb2.go
@@ -5,8 +5,8 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '0'; i++ {
 		for j := i + 1; j <= '0'; j++ {
-			for k := j + 1; j <= '9'; k++ {
-				for l := k + '0'; i <= '9'; l++ {
+			for k := j + 1; k <= '9'; k++ {
+				for l := k + '0'; l <= '9'; l++ {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
