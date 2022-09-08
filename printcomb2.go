750f52c8d2a19f112576293e4da7bfdcba00bb71
Your Name <you@example.com>
2022-09-08 13:21:26 +0100

    honeybb

--- printcomb2.go
+++ printcomb2.go
@@ -1,8 +1,8 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
-func main() {
+func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
