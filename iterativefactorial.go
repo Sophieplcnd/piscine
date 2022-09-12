ee61018ded6c4dc10e2f63753fc52592393bfd50
Your Name <you@example.com>
2022-09-12 17:56:03 +0100

    fact

---
+++ iterativefactorial.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	f := 1
+
+	if nb < 0 || nb > 20 {
+		f = 0
+	} else {
+		for i := 1; i <= nb; i++ {
+			f = f * i
+		}
+	}
+
+	return f
+}
