402e46d59f980ff086f8f61551203b74b09d93c6
Your Name <you@example.com>
2022-09-12 18:06:52 +0100

    fact2

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,11 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	f := 1
+	f := 1 // factorial variable
 
-	if nb < 0 || nb > 20 {
-		f = 0
-	} else {
+	if nb < 0 || nb > 20 { // conditional statement testing for errors
+		f = 0 // have to return factorial as 0
+	} else { // factorial
 		for i := 1; i <= nb; i++ {
 			f = f * i
 		}
