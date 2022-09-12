a4483cf128db28cfe7d6ffc9f3efd4d0fc02cc44
Your Name <you@example.com>
2022-09-12 18:47:04 +0100

    fact2

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,7 +1,13 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb == 1 || nb == 0 { // base case
+	if nb < 0 || nb > 9999 { // base case
+		return 0
+	} else if nb == 0 {
+		return 1
+	}
+
+	if nb == 1 {
 		return 1
 	}
 
