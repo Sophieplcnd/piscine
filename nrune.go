546f8a0cd64270357732a0bc64bd8b3efae2a5a4
Your Name <you@example.com>
2022-09-14 17:17:31 +0100

    rune

--- nrune.go
+++ nrune.go
@@ -3,8 +3,8 @@ package piscine
 func NRune(s string, n int) rune {
 	a := []rune(s)
 
-	if n > 0 && n < len(a) {
-		return a[n-1]
+	if n <= 0 || n > len(a) {
+		return 0
 	}
-	return 0
+	return a[n-1]
 }
