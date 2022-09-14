f5b83dee9d42a39876759b4ae7cb5afb307e1c1a
Your Name <you@example.com>
2022-09-14 20:45:06 +0100

    s5

---
+++ isalpha.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsAplha(s string) bool {
+	letter := []rune(s)
+	for i := 0; i < len(letter); i++ {
+		if (s[i] < 65 || s[i] > 90) && (s[i] < 97 || s[i] > 122) && (s[i] < 48 || s[i] > 57) {
+			return false
+		}
+	}
+	return true
+}
