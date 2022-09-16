6dbef31391f3842f3e88fb6b13013ed99edce441
Your Name <you@example.com>
2022-09-16 01:49:41 +0100

    tovelo

--- toupper.go
+++ toupper.go
@@ -3,10 +3,10 @@ package piscine
 func ToUpper(s string) string {
 
 	ToUpper := []rune(s)
-	string := " "
+	string := ""
 
 	for i := 0; i < len(s); i++ {
-		a := ToUpperRune[i]
+		a := ToUpper(Rune[i])
 		if a >= 97 && a < 123 {
 			a -= 32
 		}
