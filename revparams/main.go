bcc4cb77b24bcf0ed16e359bd8f8a7308641c116
Your Name <you@example.com>
2022-09-20 19:19:54 +0100

    para

---
+++ revparams/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	parameters := os.Args
+	counter := 0
+	for index := range parameters {
+		counter = index
+	}
+	for i := counter; i >= 1; i-- {
+		for _, word := range parameters[i] {
+			z01.PrintRune(word)
+		}
+		z01.PrintRune('\n')
+	}
+}
