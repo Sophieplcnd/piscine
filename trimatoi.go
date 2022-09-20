f717967073e8ae36b3465bef689138b6964708d2
Your Name <you@example.com>
2022-09-20 19:09:09 +0100

    toi

---
+++ trimatoi.go
@@ -0,0 +1,16 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	runes := []rune(s)
+	aString := ""
+	check := false
+	for i := 0; i < len(runes); i++ {
+		if runes[i] == 45 && !check {
+			aString = "-"
+		} else if runes[i] >= 48 && runes[i] <= 57 {
+			check = true
+			aString += string(runes[i])
+		}
+	}
+	return Atoi(aString)
+}
