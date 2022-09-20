7400dd1e245c26b7b53e129b11480eb046023689
Your Name <you@example.com>
2022-09-20 19:15:17 +0100

    pri

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	args1 := os.Args
-	for index, value := range args1 {
-		if index != 0 {
-			for _, value := range value {
-				z01.PrintRune(value)
-			}
-			z01.PrintRune('\n')
+	myProgramName := os.Args[0]
+	s := []rune(myProgramName)
+	for index, name := range s {
+		// if index not > 1, the print will include ./, the index 0 will be ., and the index 1 will be /
+		if index > 1 {
+			z01.PrintRune(name)
 		}
 	}
+	z01.PrintRune('\n')
 }
