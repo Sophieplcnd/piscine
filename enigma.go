5b531e56f3563bc69ad84533c1f477dfbfd8f97b
Your Name <you@example.com>
2022-09-23 01:21:51 +0100

    cran

---
+++ enigma.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	CranberryC := *******c
+	CranberryD := ****d
+	CranberryB := *b
+	CranberryA := ***a
+	*******c = CranberryA
+	****d = CranberryC
+	*b = CranberryD
+	***a = CranberryB
+}
